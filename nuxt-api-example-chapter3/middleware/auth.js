ca7dfe9aed1b21c9c65a4639e9e6fe6f4ae6f45b
tichikawa14 <gakusei.umeeee@gmail.com>
2020-09-12 16:33:52 +0900

    3章完了 Nuxt.jsの機能の活用

---
+++ nuxt-api-example-chapter3/middleware/auth.js
@@ -0,0 +1,18 @@
+import Cookies from 'universal-cookie'
+
+export default ({ req, route, redirect }) => {
+  if(['/'].includes(route.path)) {
+    return
+  }
+  const cookies = req ? new Cookies(req.headers.cookie) : new Cookies()
+  const credential = cookies.get('credential')
+  console.log(credential)
+
+
+  if (credential && route.path === '/login') {
+    return redirect('/')
+  }
+  if(!credential && route.path !== '/login') {
+    return redirect('/login')
+  }
+}
---
+++ nuxt-api-example-chapter3/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/login">ログインページへ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/authed-route">認証が必要なページへ</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
